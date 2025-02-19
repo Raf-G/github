30fbceb811de4627f46cca7393052a6b4cf2dbd9
gasanov.r <Gasanov.R@uremont.com>
2025-02-19 22:12:50 +0300

    add tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 import { Profile } from "./pages/Profile";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Profile />
+        <Tabs defaultActiveKey="profile" id="homeMenu" className="mb-3">
+          <Tab eventKey="profile" title="Профиль">
+            <Profile />
+          </Tab>
+          <Tab eventKey="repositories" title="Репозитории">
+            Репозитории
+          </Tab>
+        </Tabs>
       </div>
     </div>
   );
