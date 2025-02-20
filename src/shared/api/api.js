d47f1e7ca4df34e13987d1448464170cac5cb224
gasanov.r <Gasanov.R@uremont.com>
2025-02-20 23:22:10 +0300

    change

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -2,4 +2,4 @@ export const API_BASE_URL = "https://api.github.com";
 export const API_USER_REPOS_URL = `${API_BASE_URL}/user/repos`;
 export const API_REPOS_URL = `${API_BASE_URL}/repos`;
 export const API_USERS_URL = `${API_BASE_URL}/users`;
-export const API_PERSONAL_ACCESS_TOKENS_URL = `${API_BASE_URL}/settings/personal-access-tokens`;
+export const API_PERSONAL_ACCESS_TOKENS_URL = `https://github.com/settings/personal-access-tokens`;
