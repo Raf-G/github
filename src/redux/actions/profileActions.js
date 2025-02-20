148a4974f27f903a451abeb38915e212c216538a
gasanov.r <Gasanov.R@uremont.com>
2025-02-20 18:01:44 +0300

    add func save user

--- src/redux/actions/profileActions.js
+++ src/redux/actions/profileActions.js
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 export const setLogin = (login) => ({
   type: "SET_LOGIN",
   payload: login,
@@ -14,4 +16,32 @@ export const changeLogin = (login) => {
 
 export const changeApiKey = (apiKey) => {
   return async (dispatch) => dispatch(setApiKey(apiKey));
+};
+
+export const saveUser = async (login, apiKey) => {
+  if (login && apiKey) {
+    try {
+      const response = await fetch(`https://api.github.com/users/${login}`, {
+        headers: {
+          Authorization: `token ${apiKey}`,
+        },
+      });
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          toast.error("Неверный API Key!");
+        } else if (response.status === 404) {
+          toast.error("Пользователь не найден!");
+        } else {
+          toast.error(`Ошибка: ${response.status}`);
+        }
+      }
+
+      const data = await response.json();
+      console.log(data);
+      toast.success('Данный сохранены!')
+    } catch (err) {
+      toast.error(`Произошла ошибка!`);
+    }
+  }
 };
